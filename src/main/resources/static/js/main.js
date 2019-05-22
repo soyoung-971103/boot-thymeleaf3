cf4fb5a42ea6eb946b3a87ee744188585e464e30
soyoung <th97_@naver.com>
2019-05-22 17:40:38 +0900

    commit 190522

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -18,7 +18,7 @@ function fire_ajax_submit() {
         formArray[n['name']] = n['value']; // key - value
     });
 
-    var json =JSON.stringify(formArray)
+    var json =JSON.stringify(formArray);
     console.log(json);
     $.ajax({
         type: 'post',
@@ -28,7 +28,7 @@ function fire_ajax_submit() {
         dataType: 'text',
         success: function (formArray) {
         	console.log("SUCCESS : ", formArray);
-        	window.location.href = "/users";
+        	window.location.href = "/users";	//get방식
         	
         },
         error: function (e) {
