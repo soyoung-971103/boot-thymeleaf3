279b1511e7c6bef8611167f77cb8b43d3698d498
ysy <th97_@naver.com>
2019-06-05 18:01:22 +0900

    question 추가중

--- src/main/java/idu/cs/repository/QeustionRepository.java
+++ src/main/java/idu/cs/repository/QeustionRepository.java
@@ -5,19 +5,18 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import idu.cs.entity.QuestionEntity;
 import idu.cs.entity.UserEntity;
 
 @Repository
 public interface QeustionRepository 
-	extends JpaRepository<UserEntity, Long> {
+	extends JpaRepository<QuestionEntity, Long> {
 	//findById, save, delete 선언없어도 구현 가능
 	// 아래 메소드들은 선언해야 JPA 규첵에 의해 구현됨
 	// find - select문, By - where, Order By - order by, ASC와 DESC를 함께 사용 
-	List<UserEntity> findByName(String name);
 	//id 내림차순으로 정렬
 	//List<UserEntity> findByOrderByIdDESC(String name);
-	List<UserEntity> findByCompany(String name);
-	
-	UserEntity findByUserId(String userId);//ByUserId == "where userId == "
+	//findbyid는 이미 만들어져있음
+	QuestionEntity findByWriter(String userId);//ByUserId == "where userId == "
 	
 }
